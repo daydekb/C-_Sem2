9ed6551f1b1f715eb32fbef4bc9362b4a5b63a1d
Dmitriu Ydin <da-ekb@mail.ru>
2024-01-18 18:23:16 +0500

    add Lesson_2: Task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 100;
+int[] arr = new int[n];
+int i = 0;
+
+while (1 < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.Write(arr[i]);
+    //Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
